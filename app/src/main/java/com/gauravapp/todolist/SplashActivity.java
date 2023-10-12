4efc2aca191c4b54cebf8205c537dd24001a89bd
gaurav-savaimul <gauravvs4729@gmail.com>
2023-10-12 22:23:27 +0530

    final commit

---
+++ app/src/main/java/com/gauravapp/todolist/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.gauravapp.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.vazidsapplication.to_dolistify.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },3000);
+
+    }
+}
